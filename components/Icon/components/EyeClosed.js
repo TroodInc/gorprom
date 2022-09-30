fe4e46bacabd420c1b0a55ac54a5b01cf4919feb
Irin <iraantonova1998@list.ru>
2022-09-30 15:03:23 +0300

    EyeClosed

---
+++ components/Icon/components/EyeClosed.js
@@ -0,0 +1,23 @@
+/* eslint-disable max-len */
+import React from "react";
+
+const EyeClosed = () => (
+  <svg
+    version="1.1"
+    id="Слой_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 24 24"
+    style="enable-background:new 0 0 24 24;"
+    xml:space="preserve"
+  >
+    <path
+      class="st0"
+      d="M12,5C7,5,2.7,8.1,1,12.5C2.7,16.9,7,20,12,20s9.3-3.1,11-7.5C21.3,8.1,17,5,12,5z"
+    />
+  </svg>
+);
+
+export default EyeClosed;
