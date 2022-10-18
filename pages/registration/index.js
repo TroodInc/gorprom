4b3126ce7a3f5c2b54dfa5a755183ea311728fbf
LosyashBOI <82841870+LosyashBOI@users.noreply.github.com>
2022-10-18 11:17:51 +0300

    Verification (#7)
    * changed input component styles
    
    * edited label
    
    * updated input styles
    
    * changed password check styles
    
    * added verification block
    
    * verification refactoring

--- pages/registration/index.js
+++ pages/registration/index.js
@@ -25,6 +25,8 @@ const Registration = ({ host }) => {
 
   const globalError = form.get('errors.globalError')
 
+  const verify = true
+
   return (
     <div className={styles.root}>
       <Head>
@@ -32,8 +34,8 @@ const Registration = ({ host }) => {
       </Head>
       <div className={styles.left}>
         {[
-          { title: 'Регистрация', active: true },
-          { title: 'Верификация', active: false },
+          { title: 'Регистрация', active: !verify },
+          { title: 'Верификация', active: verify },
         ].map(({ title, active }, i) => (
           <div key={i} className={classNames(styles.step, active && styles.active)}>
             {i + 1}. {title}
@@ -41,60 +43,78 @@ const Registration = ({ host }) => {
         ))}
       </div>
       <div className={styles.right}>
-        <Input
-          className={styles.login}
-          label="Почта"
-          type={INPUT_TYPES.email}
-          value={form.get('data.login')}
-          errors={form.get('errors.login')}
-          validate={{ required: true }}
-          onChange={(value) => form.set('data.login', value)}
-          onInvalid={(value) => form.set('errors.login', value)}
-          onValid={() => form.set('errors.login', [])}
-        />
-        <Input
-          className={styles.password}
-          label="Пароль"
-          type={INPUT_TYPES.password}
-          value={form.get('data.password')}
-          errors={form.get('errors.password')}
-          validate={{ required: true }}
-          onChange={(value) => form.set('data.password', value)}
-          onInvalid={(value) => form.set('errors.password', value)}
-          onValid={() => form.set('errors.password', [])}
-        />
-        <PasswordCheck
-          password={form.get('data.password')}
-          minLength={8}
-          checkLower
-          checkUpper
-          checkNumber
-          onValidate={(value) => form.set('errors.passwordCheck', value)}
-        />
-        <Button
-          className={styles.loginButton}
-          label="Продолжить"
-          disabled={form.hasErrors}
-          onClick={() => {
-            form.submit(authApiPath + 'register/', 'POST')
-              .then(({ data }) => {
-                store.setAuthData(data?.data)
-                router.push('/')
-                store.deleteFormStore(formStoreName)
-              })
-          }}
-        />
-        {globalError && (
-          <div className={styles.globalError}>{globalError}</div>
-        )}
-        <div className={styles.registerQuestion}>Уже есть аккаунт?</div>
-        <Button
-          className={styles.registerButton}
-          label="Войти"
-          type={BUTTON_TYPES.text}
-          color={BUTTON_COLORS.orange}
-          link="/login"
-        />
+        {verify &&
+            <>
+              <div className={styles.verificationText}>
+                  Мы отправили письмо вам на почту. Перейдите по ссылке из письма чтобы завершить регистрацию.
+              </div>
+              <div className={styles.confirmationText}>ожидаем подтверждения...</div>
+              <div className={styles.verificationQuestion}>Не получили письмо?</div>
+              <Button
+                className={styles.verificationButton}
+                label="Отправить повторно"
+                type={BUTTON_TYPES.text}
+                color={BUTTON_COLORS.orange}
+                onClick={() => console.warn('Предупреждающее сообщение')}
+              />
+            </>}
+        {!verify &&
+            <>
+              <Input
+                className={styles.login}
+                label="Почта"
+                type={INPUT_TYPES.email}
+                value={form.get('data.login')}
+                errors={form.get('errors.login')}
+                validate={{ required: true }}
+                onChange={(value) => form.set('data.login', value)}
+                onInvalid={(value) => form.set('errors.login', value)}
+                onValid={() => form.set('errors.login', [])}
+              />
+              <Input
+                className={styles.password}
+                label="Пароль"
+                type={INPUT_TYPES.password}
+                value={form.get('data.password')}
+                errors={form.get('errors.password')}
+                validate={{ required: true }}
+                onChange={(value) => form.set('data.password', value)}
+                onInvalid={(value) => form.set('errors.password', value)}
+                onValid={() => form.set('errors.password', [])}
+              />
+              <PasswordCheck
+                password={form.get('data.password')}
+                minLength={8}
+                checkLower
+                checkUpper
+                checkNumber
+                onValidate={(value) => form.set('errors.passwordCheck', value)}
+              />
+              <Button
+                className={styles.loginButton}
+                label="Продолжить"
+                disabled={form.hasErrors}
+                onClick={() => {
+                  form.submit(authApiPath + 'register/', 'POST')
+                    .then(({ data }) => {
+                      store.setAuthData(data?.data)
+                      router.push('/')
+                      store.deleteFormStore(formStoreName)
+                    })
+                }}
+              />
+              {globalError && (
+                <div className={styles.globalError}>{globalError}</div>
+              )}
+              <div className={styles.registerQuestion}>Уже есть аккаунт?</div>
+              <Button
+                className={styles.registerButton}
+                label="Войти"
+                type={BUTTON_TYPES.text}
+                color={BUTTON_COLORS.orange}
+                link="/login"
+              />
+            </>}
       </div>
     </div>
   )
