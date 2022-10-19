4f0b0b92fffebb87f77335d5c0386719eac7392b
LosyashBOI <kovtun000@inbox.ru>
2022-10-19 15:50:00 +0300

    added SubMenu component

---
+++ components/SubMenu/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './index.module.css'
+import Link from '../Link'
+
+const SubMenu = ({ arr }) => {
+  return (
+    <div className={styles.root}>
+      {arr.map(({ link, title }, index) => (
+        <Link href={link} key={index} className={styles.link}>
+          <div className={styles.title}>
+            {title}
+          </div>
+          <div className={styles.border} />
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+SubMenu.propTypes = {
+  arr: PropTypes.arrayOf(PropTypes.object),
+}
+
+export default SubMenu
