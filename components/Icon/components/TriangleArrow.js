52adff86950b685d101c579a12519fc0a3c677d4
LosyashBOI <kovtun000@inbox.ru>
2022-11-10 17:40:42 +0300

    changed TriangleArrow icon and added border color

--- components/Icon/components/TriangleArrow.js
+++ components/Icon/components/TriangleArrow.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const TriangleArrow = () => (
-  <svg x="0px" y="0px" viewBox="0 0 512 512">
-    <polygon points="256,112 512,400 0,400" />
+  <svg width="16" height="9" viewBox="0 0 16 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M7.86805e-07 -1.39876e-06L3.83233 -1.06373e-06L8.01597
+        6.02325L12.1996 -3.32241e-07L16 0L9.61277 9L6.41916
+        9L7.86805e-07 -1.39876e-06Z" fill="#2F2D2D"
+    />
   </svg>
 )
 
