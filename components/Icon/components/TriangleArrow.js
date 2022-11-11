b2c54c4ed5c5e35156cbf7abdd9e3a2ca0f47172
LosyashBOI <kovtun000@inbox.ru>
2022-11-11 11:13:51 +0300

    removed fill styles from svg

--- components/Icon/components/TriangleArrow.js
+++ components/Icon/components/TriangleArrow.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const TriangleArrow = () => (
-  <svg width="16" height="9" viewBox="0 0 16 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="16" height="9" viewBox="0 0 16 9" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M7.86805e-07 -1.39876e-06L3.83233 -1.06373e-06L8.01597
         6.02325L12.1996 -3.32241e-07L16 0L9.61277 9L6.41916
