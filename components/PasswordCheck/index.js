5347f98c689aac34b0a64acecee0bf92e092d03b
LosyashBOI <82841870+LosyashBOI@users.noreply.github.com>
2022-11-14 12:36:30 +0300

    Registration (#20)
    * changed PasswordCheck component
    
    * changed registration page
    
    * changed colors

--- components/PasswordCheck/index.js
+++ components/PasswordCheck/index.js
@@ -17,48 +17,57 @@ const PasswordCheck = ({
 
   if (minLength) {
     const lengthErr = password.length < minLength ?
-      { title: `Как минимум ${minLength} символов`, icn: ICONS_TYPES.clear, style: styles.clear } :
-      { title: `Как минимум ${minLength} символов`, icn: ICONS_TYPES.confirm, style: styles.check }
+      {
+        title: `Как минимум ${minLength} символов`,
+        icn: ICONS_TYPES.clear,
+        iconStyle: styles.clear,
+      }
+      :
+      {
+        title: `Как минимум ${minLength} символов`,
+        icn: ICONS_TYPES.confirm,
+        iconStyle: styles.check,
+      }
 
     errorsArr.push(lengthErr)
   }
 
   if (checkLower) {
     const lowerErr = /[a-z]/g.test(password) ?
-      { title: 'Маленькие буквы (a-z)', icn: ICONS_TYPES.confirm, style: styles.check } :
-      { title: 'Маленькие буквы (a-z)', icn: ICONS_TYPES.clear, style: styles.clear }
+      { title: 'Маленькие буквы (a-z)', icn: ICONS_TYPES.confirm, iconStyle: styles.check } :
+      { title: 'Маленькие буквы (a-z)', icn: ICONS_TYPES.clear, iconStyle: styles.clear }
 
     errorsArr.push(lowerErr)
   }
 
   if (checkUpper) {
     const upperErr = /[A-Z]/g.test(password) ?
-      { title: 'Заглавные буквы (A-Z)', icn: ICONS_TYPES.confirm, style: styles.check } :
-      { title: 'Заглавные буквы (A-Z)', icn: ICONS_TYPES.clear, style: styles.clear }
+      { title: 'Заглавные буквы (A-Z)', icn: ICONS_TYPES.confirm, iconStyle: styles.check } :
+      { title: 'Заглавные буквы (A-Z)', icn: ICONS_TYPES.clear, iconStyle: styles.clear }
 
     errorsArr.push(upperErr)
   }
 
   if (checkNumber) {
     const numErr = /[0-9]/g.test(password) ?
-      { title: 'Числа (0-9)', icn: ICONS_TYPES.confirm, style: styles.check } :
-      { title: 'Числа (0-9)', icn: ICONS_TYPES.clear, style: styles.clear }
+      { title: 'Числа (0-9)', icn: ICONS_TYPES.confirm, iconStyle: styles.check } :
+      { title: 'Числа (0-9)', icn: ICONS_TYPES.clear, iconStyle: styles.clear }
 
     errorsArr.push(numErr)
   }
 
   if (checkSpec) {
     const specErr = /\W/g.test(password) ?
-      { title: 'Без спец. символов', icn: ICONS_TYPES.clear, style: styles.clear } :
-      { title: 'Без спец. символов', icn: ICONS_TYPES.confirm, style: styles.check }
+      { title: 'Без спец. символов', icn: ICONS_TYPES.clear, iconStyle: styles.clear } :
+      { title: 'Без спец. символов', icn: ICONS_TYPES.confirm, iconStyle: styles.check }
 
     errorsArr.push(specErr)
   }
 
   if (checkLetter) {
     const letterErr = /[a-z]/gi.test(password) ?
-      { title: 'Буквы (a-z)', icn: ICONS_TYPES.confirm, style: styles.check } :
-      { title: 'Буквы (a-z)', icn: ICONS_TYPES.clear, style: styles.clear }
+      { title: 'Буквы (a-z)', icn: ICONS_TYPES.confirm, iconStyle: styles.check } :
+      { title: 'Буквы (a-z)', icn: ICONS_TYPES.clear, iconStyle: styles.clear }
 
     errorsArr.push(letterErr)
   }
@@ -75,11 +84,13 @@ const PasswordCheck = ({
   }
 
   const renderErrors = (arr) => {
-    return arr.map(({ title, icn, style }, i) => {
+    return arr.map(({ title, icn, iconStyle }, i) => {
       return (
         <div className={styles.item} key={i}>
-          <Icon type={icn} size={11} className={style}/>
-          {title}
+          <Icon type={icn === ICONS_TYPES.clear ? '' : icn} size={16} className={iconStyle}/>
+          <div>
+            {title}
+          </div>
         </div>
       )
     })
